377c935f1f00d5971e3b100a683db39817fb3853
Andrew <luzkovskyandrey@gmail.com>
2013-06-25 01:34:54 +0300

    init commit

---
+++ public/script.js
@@ -0,0 +1,128 @@
+
+
+
+
+
+
+
+
+
+
+function GRUD(opt){
+	var self = {};
+
+var Model = Backbone.Epoxy.Model.extend({	
+	    idAttribute: "_id",
+	    defaults : opt.form.defaults
+	});
+
+var ListItemView = Backbone.View.extend({
+    tagName: "li",
+    events : {
+    	"click .edit" : "editItem",
+    	"click .delete" : "delete"
+    },
+    delete : function(e){
+    	this.model.destroy();	
+    },
+    editItem : function(e){
+    	this.model.trigger('details', this.model.get('_id'));
+    },
+
+    initialize: function() {
+    	var templateText = $("#item").html();
+    	var template = _.template(templateText);
+	    this.$el.html(template(this.model.toJSON()));  
+    }
+});
+
+var ListCollection = Backbone.Collection.extend({
+    model: Model,
+    view: ListItemView
+});
+
+
+var ListView = Backbone.Epoxy.View.extend({
+    initialize: function(opt) {
+    	var template = _.template(opt.templateText);
+    	this.$el.html(template());
+    },
+    events : {
+    	"click .add" : "newItem"
+    },
+    newItem : function(){
+    	this.collection.trigger('details');
+    },
+});
+
+	var DetailsView = Backbone.Epoxy.View.extend({
+		events : {
+			"click .save" : "saveForm",
+	    	"click .cansel" : "cansel"
+		},
+		
+		saveForm : function(e){
+			this.model.save({
+		            wait: true
+		        },{
+				success : function(model){
+					model.trigger('showList');
+				}
+			});
+		},
+		cansel : function(e){
+	    	this.model.trigger('showList');
+	    },
+	    bindingSources:  opt.form.bindingSources,
+
+	    bindings : opt.form.bindings,	    
+	    initialize: function(opt) {
+	    	var template = _.template(opt.templateText);
+	    	this.$el.html(template());
+
+	    	this.model.urlRoot = opt.resurce;
+	    }
+	});
+
+	self.show = function(view){
+		$(opt.container).empty().html(view.render().el);
+	}
+
+	self.list = function(){
+		var templateText = $('#list').html();
+
+		var collection = self.collection;
+		var listView = new ListView({ 
+			templateText : templateText,
+			resurce : opt.resurce,
+			collection : collection
+		});
+		
+		collection.url = opt.resurce;
+
+		self.show(listView);
+
+		collection.fetch();
+	}
+
+	self.details = function(id){
+		var detailsTemplate = $('#details').html();
+		var item = id ? self.collection.get(id) : new Model();
+		var form = new DetailsView({
+			templateText : detailsTemplate,
+			resurce : opt.resurce	,
+			model : item	
+		}); 
+		item.on('showList', self.list, self);
+		self.show(form);
+	}
+
+	self.start = function(init){
+		init();
+		self.collection = new ListCollection();
+		self.collection.on('details', self.details, self);
+		self.list();
+	}
+
+	return self;
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+var	express = require('express')
+	, app = express()
+	, path = require('path');
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser()); 
+app.use(express.static(path.join(__dirname, 'public') ));
+
+app.use(express.session({ secret: 'zzzzzzz'} ));
+
+var id = 3;
+var items = [
+		{ name : 'vasa', _id : '1', typeId : '2' },
+		{ name : 'peta', _id : '2' , typeId : '2'},
+		{ name : 'gala', _id : '3' , typeId : '1'}
+	];
+
+app.get('/api/events', function(req, res){
+	res.json(items);
+});
+
+app.post('/api/events', function(req, res){
+	var newItem = req.body;
+	newItem._id = ++id;
+	items.push(newItem);
+	res.json(newItem);	
+});
+
+app.put('/api/events/:id',function(req, res){
+	var item = req.body;
+	var id = req.params["id"];
+	for (var i = items.length - 1; i >= 0; i--) {
+		if (items[i]._id == id){
+			items[i].name = item.name;
+			items[i].typeId = items.typeId;
+		}
+	};
+
+	res.json(item);
+});
+
+app.delete('/api/events/:id',function(req, res){
+	var item = req.body;
+	var id = req.params["id"];
+	var index = -1;
+	for (var i = 0; i < items.length; i++) {
+		if (items[i]._id == id)
+			index = i;
+	};
+
+	items.splice(index, 1)
+
+	res.json(item);
+});
+
+
+app.get('/api/types', function(req, res){
+	res.json([{label:"type1", value:"1"}, {label:"type2", value:"2"}]);
+})
+
+//app.resource('/api/events', db.Event);
+
+app.listen(8080, function(){
+  console.log("Express server listening on port %d", '8080');
+});
